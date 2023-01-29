a0496c12e822cdfc09e491b13762e6c6b341fc41
Asna Majeed <85230276+Asna1408@users.noreply.github.com>
2023-01-29 14:27:10 +0530

    Add files via upload

---
+++ roll dice.py
@@ -0,0 +1,11 @@
+import random
+def rolldice(min,max):
+    while True:
+        print("rolling dice...")
+        number = random.randint(min,max)
+        print("Your number : {number}")
+        choice = input("Do you want to roll the dice again? (y/n)")
+        if choice.lower() == "n":
+            break
+
+rolldice (1,6)
